63a0dca7474e1a2fad67b2c14e57172f16aa7378
IAmDeanDev <deandpersonal@gmail.com>
2025-06-21 14:31:10 +1000

    Added a spirit class, and a test spirit weapon
    Spirit Class
    Spirit Weapon
    Invisible Spirit Projectile

---
+++ TracerWizard/Content/Items/Weapons/PreHardmode/SpiritClass/SpiritTestWeapon.cs
@@ -0,0 +1,46 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TracerWizard.Content.DamageClasses;
+using TracerWizard.Content.Projectiles;
+
+namespace TracerWizard.Content.Items.Weapons.PreHardmode.SpiritClass
+{
+    public class SpiritTestWeapon : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            Item.staff[Type] = true;
+        }
+
+
+        public override void SetDefaults()
+        {
+            Item.width = 140;
+            Item.height = 21;
+
+            Item.DamageType = ModContent.GetInstance<Spirit>();
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.channel = true;
+            Item.useTime = 2;
+            Item.useAnimation = 2;
+            Item.autoReuse = true;
+            Item.mana = 0;
+            Item.crit = 10;
+            Item.shoot = ModContent.ProjectileType<InvisibleProjectile>();
+
+            Item.damage = 5;
+            Item.knockBack = 1;
+            Item.crit = 5;
+            Item.value = Item.sellPrice(gold: 1);
+            Item.rare = ItemRarityID.Purple;
+
+            Item.noUseGraphic = true;
+
+        }
+    }
+}
---
+++ TracerWizard/Content/Projectiles/InvisibleProjectile.cs
@@ -0,0 +1,48 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TracerWizard.Content.DamageClasses;
+
+namespace TracerWizard.Content.Projectiles
+{
+    public class InvisibleProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.NoLiquidDistortion[Type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 25;
+            Projectile.height = 25;
+
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = ModContent.GetInstance<Spirit>();
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 2;
+            Projectile.light = 0.25f;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+
+            Projectile.alpha = 255;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<InvisibleProjectile>()] > 0)
+            {
+                Projectile.Kill();
+                return;
+            }
+
+            Projectile.timeLeft = 2;
+            Projectile.Center = Main.MouseWorld;
+            Projectile.netImportant = true;
+            Projectile.netUpdate = true;
+        }
+    }
+}
